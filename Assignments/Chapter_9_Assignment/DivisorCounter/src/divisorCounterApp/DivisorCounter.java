775c100c9f349dbd45a563fff2ea785adac9a364
Prashant  Singh <prashant.s@intimetec.com>
2025-06-03 08:52:27 +0530

    Test  Cases for negative input

--- Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/DivisorCounter.java
+++ Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/DivisorCounter.java
@@ -3,22 +3,10 @@ package divisorCounterApp;
 public class DivisorCounter {
 
     public boolean isDivisor(int num, int i) {
-        if (num <= 0) {
-            throw new IllegalArgumentException("Input must be a positive integer");
-        }
-        return num % i == 0;
+        
     }
 
     public int countMatchingDivisors(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("Input must be a positive integer");
-        }
-        int result = 0;
-        for (int i = 1; i < n; i++) {
-            if (isDivisor(n,i) && isDivisor(n + 1,i)) {
-                result++;
-            }
-        }
-        return result;
+        
     }
 }
--- Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/Main.java
+++
@@ -1,9 +0,0 @@
-package divisorCounterApp;
-
-public class Main {
-    public static void main(String[] args) {
-        DivisorCounter divisorCounter = new DivisorCounter();
-        int count = divisorCounter.countMatchingDivisors(100);
-        System.out.println("Count: " + count);
-    }
-}
