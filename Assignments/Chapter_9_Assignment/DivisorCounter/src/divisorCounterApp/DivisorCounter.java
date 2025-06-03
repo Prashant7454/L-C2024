e8d27c0c56ec28ebeacbf2fd01d84584a699ea12
Prashant  Singh <prashant.s@intimetec.com>
2025-06-03 08:54:14 +0530

    Method throw error message for negative input

--- Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/DivisorCounter.java
+++ Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/DivisorCounter.java
@@ -3,10 +3,16 @@ package divisorCounterApp;
 public class DivisorCounter {
 
     public boolean isDivisor(int num, int i) {
-        
+        if (num <= 0) {
+            throw new IllegalArgumentException("Input must be a positive integer");
+        }
+        return 0;
     }
 
     public int countMatchingDivisors(int n) {
-        
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input must be a positive integer");
+        }
+        return 0;
     }
 }
