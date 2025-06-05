c62c2fbc69f5f3f0afc9003b1daf264c3ccf8bda
Prashant  Singh <prashant.s@intimetec.com>
2025-06-05 17:23:35 +0530

    Updated the code with input validation

--- Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/Main.java
+++ Assignments/Chapter_9_Assignment/DivisorCounter/src/divisorCounterApp/Main.java
@@ -3,14 +3,25 @@ package divisorCounterApp;
 import java.util.Scanner;
 
 public class Main {
+
+    final static int MAX_NUMBER_OF_TESTCASES = 1000000;
+    final static int MAX_TESTCASE_INPUT = 10000000;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int testCasesNumber = scanner.nextInt();
 
+        if(testCasesNumber<1 || testCasesNumber> MAX_NUMBER_OF_TESTCASES){
+            System.out.println("Test Cases Should be in range 1 to " + MAX_NUMBER_OF_TESTCASES);
+            return;
+        }
+
         int[] testCases = new int[testCasesNumber];
         for(int index = 0; index < testCasesNumber; index++){
             testCases[index] = scanner.nextInt();
+            if(testCases[index]> MAX_TESTCASE_INPUT){
+                System.out.println("Input should be in range 1 to " + MAX_TESTCASE_INPUT);
+            }
         }
 
         DivisorCounter divisorCounter = new DivisorCounter();
